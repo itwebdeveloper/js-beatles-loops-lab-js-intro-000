07f94869a02708067dd477c895a5f5d40ee57283
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 22:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = Array();
+  for (let i = 0; i < musicians.length; i++) {
+    array[i] = `"${musicians[i]} plays ${instruments[i]}"`; 
+  }
+  return array;
+}
